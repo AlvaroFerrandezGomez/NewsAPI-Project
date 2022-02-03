c6c8ef8545674d123061e81661be0c7e4ff8dc7f
Álvaro Ferrández <aferrandezgomez@gmail.com>
2022-02-03 21:24:03 +0100

    test

--- NewsAPI Project/ViewController.swift
+++ NewsAPI Project/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func vi/Users/aferrandez/mmm/NewsAPI-Project/NewsAPI Project/SceneDelegate.swiftewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
